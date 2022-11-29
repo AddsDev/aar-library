bd3e38483605f11b291f5fd4a424303fd3cf40c3
Adrian Ruiz <adrian.ruiz@evertecinc.com>
2022-11-29 15:55:58 -0500

    Initial commit

---
+++ app/src/main/java/dev/adds/aar_library/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.adds.aar_library
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
